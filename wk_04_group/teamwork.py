b67ed0fd3bd80f848252071c51d6e6f39c659ef3
Nathaniel-Carl-Peter <144785279+Nathaniel-Carl-Peter@users.noreply.github.com>
2023-10-03 20:05:29 +1000

    Add files via upload

---
+++ wk_04_group/teamwork.py
@@ -0,0 +1,17 @@
+def main():
+    numbers = get_numbers()
+    square_numbers(numbers)
+    display_number(numbers)
+
+
+def get_numbers(numbers):
+    numbers = int(input(f"Enter numbers separated by commas: "))
+    return numbers
+
+
+def square_numbers(number):
+
+
+
+def display_number(numbers):
+    """Display numbers"""
